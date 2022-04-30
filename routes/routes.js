4c6edff924b50ab23dd455135d907fa85d89623c
Rob Benney <rob.benney@gmail.com>
2022-04-30 16:32:44 -0400

    get and post to routes file

--- routes/routes.js
+++ routes/routes.js
@@ -2,31 +2,36 @@ const fs = require('fs');
 const path = require('path');
 
 // notes variable
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
 
 
 // GET method route
 app.get('/api/notes', (req, res) => {
     res.send(notes)
-})
+    })
   
 // POST method route
-  app.post('/api/notes', (req, res) => {
-    res.send('POST request to the homepage')
-})
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log('Added new note: '+newNote.title)
+    })
 
 // get notes with a specific id
 app.get('/api/notes/:id', (req, res) => {
-    const newNote = findById(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
-      res.send(404);
-    }
-  });
+    res.json(notes[req.params.id]);
+    });
 
 // delete notes with a specific id
-
-
+app.delete("/api/notes/:id", (req, res) {
+    notes.splice(req.params.id, 1);
+    updateDb();
+    console.log("Deleted not with id "+req.params.id);
+    });
+});
 // display notes html page
 
 
