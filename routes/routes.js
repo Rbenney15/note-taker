6545de66d616bfbf5f942c48ace3386be13c829b
Rob Benney <rob.benney@gmail.com>
2022-04-30 12:38:14 -0400

    server file setup

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+// notes variable
+// get routes
+// post route
+// get notes with a specific id
+// delete notes with a specific id
+// display notes html page
+// display index html 
+// update json when a note is added or deleted
